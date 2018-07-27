b131567cc51efd19bad31f66120d784a6230c367
nyanten <190372k@ugs.kochi-tech.ac.jp>
2018-07-27 12:46:10 +0900

    update

--- camera.py
+++ camera.py
@@ -4,6 +4,10 @@ import cv2
 CD = os.getcwd()
 REAL_PATH = os.path.join(CD, "image_folder", "take.jpg")
 
+FACE_CASCADE = "/usr/local/Cellar/opencv/3.4.1_5/"\
+               "share/OpenCV/haarcascades/"\
+               "haarcascade_frontalface_default.xml"
+
 EYE_CASCADE = "/usr/local/Cellar/opencv/3.4.1_5/"\
               "share/OpenCV/haarcascades/"\
               "haarcascade_eye.xml"
@@ -16,13 +20,15 @@ def Camera():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 512)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 512)
 
-    eye_cascade = cv2.CascadeClassifier(EYE_CASCADE)
+    face_cascade = cv2.CascadeClassifier(FACE_CASCADE)
+    
     glass = cv2.imread("./image_folder/glass.png")
     
     while True:
         ret, im = cap.read()
-        face = eye_cascade.detectMultiScale(im)
-        #gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        face = face_cascade.detectMultiScale(im)
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        #dst = cv2.addWeighted(im, 1, glass, 0.8, 0)
         
         key = cv2.waitKey(10)
         if key == 27:  # esc
@@ -33,10 +39,9 @@ def Camera():
             print("take a photo")
 
         for x, y, w, h in face:
-            col = (0, 0, 225)
+            col = (255, 0, 0)
             bd = 2
             cv2.rectangle(im, (x, y), (x+w, y+h), col, thickness=bd)
-            cv2.
                 
         cv2.imshow('camera capture', im)
         
